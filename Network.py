4df3afc45bcf57e0000e40fc42e5a92a982ccbec
Aiden Zhang <aidentzhang@gmail.com>
2025-08-07 20:52:03 -0400

    Everything done just need to figure out the spacing

--- Network.py
+++ Network.py
@@ -4,7 +4,6 @@ import random
 import time
 import keras
 
-#(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
 
 
 
@@ -46,7 +45,8 @@ def forwardPropagate(LayerValues, layer, network, labels):
         if layer == 0:
                 i=0
                 while i < len(LayerValues):
-                        labels[i] = LayerValues[i]
+                        if i < 5 or i > 779: 
+                                labels[i] = LayerValues[i]
                         i+=1
                 updateGraph()
 
@@ -90,8 +90,8 @@ def forwardPropagate(LayerValues, layer, network, labels):
 
 
 #INITIALIZE NETWORK STRUCT
-num_layers = 3
-nodesPerLayer = [2,2,2]
+num_layers = 4
+nodesPerLayer = [784,16,16,10]
 
 
 
@@ -121,7 +121,7 @@ i = 0
 
 #SPACING INFO
 xSpace = float(2)/(ids+2)
-ySpace = float(2)/(max(nodesPerLayer))
+ySpace = float(2)/(16 if max(nodesPerLayer) > 16 else max(nodesPerLayer))
 pos = {}
 currX = -1+xSpace
 i = 0
@@ -150,7 +150,8 @@ while i < num_layers-1:
 
                 for next in network[i+1]:
                         temp.append(connection(random.random()*2-1, i, curr[0].neuron, next[0].neuron, curr[0].id, next[0].id))
-                        G.add_edge(curr[0].id, next[0].id)
+                        if curr[0].id >779 or curr[0].id < 5:
+                                G.add_edge(curr[0].id, next[0].id)
                 network[i][curr[0].neuron].append(temp)
         i+=1
 
@@ -198,7 +199,7 @@ def find_Gradient(layer, updateList, expected_outcomes):
                                 delC_delA = 0
                                 i = 0
                                 while i < len(updateList[layer][link.toNeuron][1]):
-                                        delC_delA+=updateList[layer][link.toNeuron][1][i]*(1/(network[layer][link.toNeuron][0].value))*network[layer][link.fromNeuron][1][i].weight
+                                        delC_delA+=updateList[layer][link.toNeuron][1][i]*(1/(network[layer][link.toNeuron][0].value))*network[layer][link.toNeuron][1][i].weight
                                         i+=1
                                 delC_delA*=(sigmoid_Derivative(network[layer][link.toNeuron][0].preSigValue)*network[layer-1][link.fromNeuron][0].value)
                                 updateList[layer-1][link.fromNeuron][1][link.toNeuron] = delC_delA
@@ -225,38 +226,61 @@ def updateNetwork(network, updateList):
         while i < len(network):
                 j = 0
                 while j < len(network[i]):
-                        network[i][j][0].bias -= updateList[i][j][0]*0.1
+                        network[i][j][0].bias -= updateList[i][j][0]*growth_Factor
                         if i != len(network)-1:
                                 k = 0
                                 while k < len(network[i][j][1]):
-                                        network[i][j][1][k].weight -= updateList[i][j][1][k]*0.1
+                                        network[i][j][1][k].weight -= updateList[i][j][1][k]*growth_Factor
                                         k+=1
                         j+=1
                 i+=1
         return network
 
-
+growth_Factor = 0.3
 import copy
-print(network)
+#print(network)
 updateList = copy.deepcopy(network)
-target = [0, 1]
-input = [0.2, 1]
-repeats = 0
-while repeats < 10:
-        answer, network = forwardPropagate(input, 0, network, labels)
-        gradient= find_Gradient(num_layers-1, updateList, target)
 
-        network = updateNetwork(network, gradient)
+
+
 
 
 
 #print(answer)
+import pdb
+(x_preprocessed, y_preprocessed), (x_test, y_preprocessed) = keras.datasets.mnist.load_data()
+#print(x_train[0])
+x_preprocessed = x_preprocessed[:500]
+y_preprocessed = y_preprocessed[:500]
+x_train = []
+x = 0
+while x < len(x_preprocessed):
+        j = 0
+        temp = []
+        while j < 28:
+                k = 0
+                while k < 28:
+                        temp.append(float(int(x_preprocessed[x][j][k]))/255)
+                        k+=1
+                j+=1
+        x+=1
+        x_train.append(temp)
+y_train = []
+x = 0
+while x < len(y_preprocessed):
+        temp = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        temp[y_preprocessed[x]] = 1
+        y_train.append(temp)
+        x+=1
 
+print(len(x_train[0])) 
+repeats = 0
+while repeats < 500:
+        answer, network = forwardPropagate(x_train[repeats], 0, network, labels)
+        gradient= find_Gradient(num_layers-1, updateList, y_train[repeats])
 
-
-
-
-
+        network = updateNetwork(network, gradient)
+        repeats+=1
 
 plt.ioff()
 plt.show()
